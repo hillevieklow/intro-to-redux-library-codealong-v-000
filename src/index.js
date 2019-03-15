9060600bdcf93e132973c5673c6ec49debdb8db3
Hillevi Eklöw <hillevieklow@gmail.com>
2019-03-15 16:12:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ import './index.css';
 
 const store = createStore(shoppingListItemReducer);
 
- ReactDOM.render(<Provider store={store}> {' '}<App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}> {' '}<App /></Provider>, document.getElementById('root'));
